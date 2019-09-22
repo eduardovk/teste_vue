bf33ce037faf76ea395f2eb091b09800e7a175e7
eduardovk <eduardo_vk@hotmail.com>
2019-09-21 22:12:51 -0300

    Criado componente Produtos.vue

--- 18_Avancado/Exemplo/src/App.vue
+++ 18_Avancado/Exemplo/src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <ProdutosExemplo></ProdutosExemplo>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld'
+import ProdutosExemplo from './components/Produtos'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    ProdutosExemplo
   }
 }
 </script>
---
+++ 18_Avancado/Exemplo/src/components/Produtos.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <p>Teste Produto</p>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'ProdutosExemplo',
+        data(){
+            return{
+
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>

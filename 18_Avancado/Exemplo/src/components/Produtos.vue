37389ce580d3f3c2da7eed6045fcae412fe6547c
eduardovk <eduardo_vk@hotmail.com>
2019-09-21 22:35:53 -0300

    Adicionado bootstrap ao projeto vue, adicionada tabela ao componente produtos.vue

--- 18_Avancado/Exemplo/src/components/Produtos.vue
+++ 18_Avancado/Exemplo/src/components/Produtos.vue
@@ -1,18 +1,38 @@
 <template>
-    <div>
-        <p>Teste Produto</p>
+    <div class="container">
+        <table class="table table-striped mt-4">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">ID</th>
+                    <th scope="col">Titulo</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="produto in produtos" :key="produto.id">
+                    <th scope="row">1</th>
+                    <td>{{produto.id}}</td>
+                    <td>{{produto.titulo}}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
 <script>
-    export default{
-        name: 'ProdutosExemplo',
-        data(){
-            return{
-
-            }
+export default{
+    name: 'ProdutosExemplo',
+    data(){
+        return{
+            produtos:[
+                {id:1, titulo:'Produto 1'},
+                {id:2, titulo:'Produto 2'},
+                {id:3, titulo:'Produto 3'},
+                {id:4, titulo:'Produto 4'}
+            ]
         }
     }
+}
 </script>
 
 <style>
